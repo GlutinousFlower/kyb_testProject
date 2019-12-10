d7450584d89c72cdc42d0556c31b68c492109ae2
luping.xiao <2337603659@qq.com>
2019-12-10 11:33:28 +0800

    封装swipeUp方法

--- common/common_fun.py
+++ common/common_fun.py
@@ -48,6 +48,14 @@ class Common(BaseView):
         x2 = int(l[0] * 0.1)
         self.swipe(x1, y1, x2, y1, 1000)
 
+    def swipeUp(self):
+        logging.info('swipeUp~')
+        l=self.get_screenSize()
+        x1 = int(l[1] * 0.5)
+        y1 = int(l[0] * 0.9)
+        y2 = int(l[0] * 0.1)
+        self.swipe(x1,y1,x1,y2,1000)
+
 
     def getTime(self):
         self.now=time.strftime('%Y-%m-%d %H:%M:%S')
